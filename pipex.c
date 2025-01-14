645153c8f62e3163342f0284af9d6655983d36e0
Ganesha O'Donnell <go-donne@student.42heilbronn.de>
2025-01-14 12:31:33 +0100

    Implementation plan, incl. main elements of program

---
+++ pipex.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 12:04:43 by go-donne          #+#    #+#             */
+/*   Updated: 2025/01/14 12:04:51 by go-donne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+pipex.c (main program):
+- main() - Entry point, argument handling
+- pipex() - Core pipe execution logic
+- execute_cmd() - Command execution handler
+*/
